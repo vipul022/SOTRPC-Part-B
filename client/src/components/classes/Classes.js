9be0992de25df55bb3d23c3add40aabd99983c3c
vipul022 <vipul.seth022@gmail.com>
2020-12-04 15:57:16 +1100

    set up globa state using createContext and useContext hooks

--- client/src/components/classes/Classes.js
+++ client/src/components/classes/Classes.js
@@ -1,6 +1,8 @@
 import React from "react";
+import ClassData from "./data/class_data";
 
 const Classes = () => {
+  // !use ClassData in use effect
   return <h1>Classes</h1>;
 };
 export default Classes;
