b1d9ba13cf33bc00c6b877ab9c3db917c3965572
vipul022 <vipul.seth022@gmail.com>
2020-12-04 13:52:40 +1100

    added routes for home and classes components

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./components/home/Home";
+import Nav from "./components/nav/Nav";
+import Classes from "./components/classes/Classes";
 
 const App = () => {
-  return <hi>hello World</hi>;
+  return (
+    <div>
+      <Router>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/classes" component={Classes} />
+        </Switch>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
