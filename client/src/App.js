cd770b6c17e2384549a6f4dffcafa145f90c315c
vipul022 <vipul.seth022@gmail.com>
2020-12-04 13:15:24 +1100

    created client folder using create react app and created home and nav folders inside components folder

---
+++ client/src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import Home from "./components/home/Home";
+
+const App = () => {
+  return <hi>hello World</hi>;
+};
+
+export default App;
