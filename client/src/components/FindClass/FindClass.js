3582f0eb8972eb82c77752db91d34e620e6569e0
vipul022 <vipul.seth022@gmail.com>
2020-12-04 15:18:51 +1100

    added link for classes component in FindClass component

--- client/src/components/FindClass/FindClass.js
+++ client/src/components/FindClass/FindClass.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const FindClass = () => {
   return (
     <div>
       <h4>Lorem ipsum dolor sit.</h4>
-      <button>Find a Class</button>
+      <Link to="/classes">
+        <button>Find a Class</button>
+      </Link>
     </div>
   );
 };
