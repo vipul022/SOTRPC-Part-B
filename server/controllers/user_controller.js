c7c46f62616a0d47d93db4d655cfbf6219b951a0
drumovski <jason.stacy@gmail.com>
2020-12-05 19:25:23 +1100

    add user to user controller

--- server/controllers/user_controller.js
+++ server/controllers/user_controller.js
@@ -1,31 +1,56 @@
+const UserModel = require("../models/user")
 
-    
 function loginUser(req, res) {
     res.send("got your loginUser request")
-}
+};
+
 function logoutUser(req, res) {
     res.send("got your logoutUser request")
-}
+};
+
 function showUsers(req, res) {
     res.send("got your showUsers request")
-}
-function addUser(req, res) {
-    res.send("got your addUser request")
-}
+};
+
+async function addUser (req, res) {
+    // res.send("got your addUser request")
+    res.json(req.body);
+    const {
+        name,
+        address,
+        phone,
+        email,
+        password
+    } = req.body;
+    const user = await UserModel.create({
+        name,
+        address,
+        phone,
+        email,
+        password
+    })
+
+    // Attach user to session
+    req.session.user = user;
+    console.log(req.session.user)
+};
+
 function deleteUser(req, res) {
     res.send("got your deleteUser request")
-}
+};
+
 function showUser(req, res) {
     res.send("got your showUser request")
-}
+};
+
 function editUser(req, res) {
     res.send("got your editUser request")
-}
+};
 
 module.exports = {
     loginUser,
     logoutUser,
-    showUsers, 
+    showUsers,
     addUser,
     deleteUser,
     showUser,
