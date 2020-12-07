91fda266e799dc9782abdee82b12dfa9e9d36673
vipul022 <vipul.seth022@gmail.com>
2020-12-07 14:54:50 +1100

    added content to Classregister component

--- client/src/components/ClassRegister/ClassRegister.js
+++ client/src/components/ClassRegister/ClassRegister.js
@@ -1,6 +1,17 @@
 import React from "react";
 
 const ClassRegister = () => {
-  return <h1>Hello</h1>;
+  return (
+    <div>
+      <h1>Class Info</h1>
+      <p>To sign up for this class,</p>
+      <p>please email harry@SOTRPC.com.au</p>
+      <p>for more information and to advise of payments made</p>
+      <p>Bank: Commonwealth</p>
+      <p>BSB: 9999</p>
+      <p>Account:77777777</p>
+      <p>Ref: Your name</p>
+    </div>
+  );
 };
 export default ClassRegister;
