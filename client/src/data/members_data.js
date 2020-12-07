c7eddd0686d013b35e7735fc317d39e58a77350d
vipul022 <vipul.seth022@gmail.com>
2020-12-08 09:24:32 +1100

    added some dummy data to test members

---
+++ client/src/data/members_data.js
@@ -0,0 +1,38 @@
+export default [
+  {
+    _id: 1,
+    name: "John Cena",
+    address: "123, fake street, Melbourne",
+    phone: "033223321",
+    email: "john@test.com",
+    paid: "Paid",
+    role: "Member",
+  },
+  {
+    _id: 2,
+    name: "Mark Waugh",
+    address: "921, main street, Melbourne",
+    phone: "033223321",
+    email: "mark@test.com",
+    paid: "not paid",
+    role: "User",
+  },
+  {
+    _id: 3,
+    name: "Steve Waugh",
+    address: "24, market street, Melbourne",
+    phone: "033223321",
+    email: "mark@test.com",
+    paid: "awaiting",
+    role: "User",
+  },
+  {
+    _id: 4,
+    name: "Ricky Ponting",
+    address: "40, bond street, Melbourne",
+    phone: "033223321",
+    email: "ricky@test.com",
+    paid: "Paid",
+    role: "Admin",
+  },
+];
