1fea729a15d5583389dfff71101c78388115a1ed
vipul022 <vipul.seth022@gmail.com>
2020-12-09 19:52:02 +1100

    created SignIn component for users to login

---
+++ client/src/components/SignIn/SignIn.js
@@ -0,0 +1,38 @@
+// import React, { useState } from "react";
+// import { useGlobalState } from "../../config/globalState";
+// const SignIn = ({ history }) => {
+//   const { dispatch } = useGlobalState();
+
+//   const initialFormState = {
+//     username: "",
+//     password: "",
+//   };
+
+//   const [userDetails, setUserDetails] = useState(initialFormState);
+
+//   const handleSubmit = (event) {
+//     event.preventDefault()
+//   }
+
+//   return (
+//     <div>
+//       <form onSubmit={handleSubmit}>
+// <div>
+//   <label>Email</label>
+//   <input
+//   required
+//   type="email"
+//   name="username"
+//   placeholder="Enter your email..."
+//   onChange={handleChange}
+//   ></input>
+// </div>
+// <div>
+
+// </div>
+//       </form>
+//     </div>
+//   );
+// };
+
+// export default SignIn;
