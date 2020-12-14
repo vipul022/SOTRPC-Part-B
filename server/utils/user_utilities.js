133a9a8a20bc44c63710a43ef249eebe98532722
drumovski <jason.stacy@gmail.com>
2020-12-14 16:35:11 +1100

    remove comments from addUserToDB

--- server/utils/user_utilities.js
+++ server/utils/user_utilities.js
@@ -6,57 +6,7 @@ async function userExists(req) {
 }
 
 const addUserToDB = function (req, res) {
-    console.log("I'm in addUserToDB")
     return new User(req.body);
-
-    // if the user doesnot exist return an error
-    // if(!user) {
-    //     return res.render("auth/login", {error: "Invalid user"})
-    // }
-    // if (User.findOne({username: req.body.username})) {
-    //     const error = ""
-    //     return error
-    // }
-
-    //     // console.log("req.body:",req.body)
-    //     // const { email, password, address, phone, role, paid, name } = req.body
-    //     // return User.register({ email, password, address, phone, role, paid, name });
-    //     // const { address, phone, role, paid, name } = req.body
-    //     // const username = req.body.email;
-    //     const {
-    //         password,
-    //         address,
-    //         phone,
-    //         role,
-    //         paid,
-    //         name
-    //     } = req.body
-    //     const username = req.body.email
-    //     const user = await User.register(new User({
-    //         username,
-    //         address,
-    //         phone,
-    //         role,
-    //         paid,
-    //         name
-    //     }), password, function (err) {
-    //         console.log("after register")
-    //         if (err) {
-    //             console.log("error")
-    //             if (err.name === 'UserExistsError') {
-    //                 res.status(409)
-    //                 res.json({
-    //                     error: err.message
-    //                 });
-    //             } else {
-    //                 res.status(500);
-    //                 res.json({
-    //                     error: err
-    //                 });
-    //             }
-    //         }
-    //     });
-    //     console.log("after the block")
 };
 
 const getUserFromDB = function (id) {
