44e963ce8e4fed65dde01d71ee8778145c9fc335
drumovski <jason.stacy@gmail.com>
2020-12-22 23:19:28 +1100

    install react bootstrap and scss

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ import SignIn from "./components/SignIn/SignIn";
 import EditClass from "./components/EditClass/EditClass";
 import Gallery from "./components/Gallery/Gallery";
 import NewPhoto from "./components/NewPhoto/NewPhoto"
-
+import "./styles/app.scss";
 const App = () => {
   const initialState = {
     classes: [],
