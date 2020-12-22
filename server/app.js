e0888f8a4cc057e359568d95f86d3d8d845a4962
drumovski <jason.stacy@gmail.com>
2020-12-22 21:17:43 +1100

    remove console.log in cors

--- server/app.js
+++ server/app.js
@@ -24,7 +24,6 @@ app.use(cors({
     origin: function (origin, callback) {
       // Check each url in whitelist and see if it includes the origin (instead of matching exact string)
       const whitelistIndex = whitelist.findIndex((url) => url.includes(origin));
-      console.log("found whitelistIndex", whitelistIndex);
       callback(null, whitelistIndex > -1);
     },
   })
