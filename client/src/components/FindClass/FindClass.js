c25cdc7819f9aab73b0248ca2f9f4660ee683a67
drumovski <jason.stacy@gmail.com>
2020-12-28 16:51:30 +1100

    add button styling

--- client/src/components/FindClass/FindClass.js
+++ client/src/components/FindClass/FindClass.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Button from "react-bootstrap/Button"
 
 const FindClass = () => {
   return (
     <div>
       <h4>Lorem ipsum dolor sit.</h4>
       <Link to="/classes">
-        <button>Find a Class</button>
+        <Button variant="primary">Find a Class</Button>
       </Link>
     </div>
   );
