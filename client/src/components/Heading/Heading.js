5581d8357f58d177e29b3b6c3249be90f05224a7
drumovski <jason.stacy@gmail.com>
2020-12-29 16:04:56 +1100

    refactor heading to use props

--- client/src/components/Heading/Heading.js
+++ client/src/components/Heading/Heading.js
@@ -3,7 +3,7 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { Link } from "react-router-dom";
 
-const Heading = (title) => {
+const Heading = ({title}) => {
 
   return (
     <div>
