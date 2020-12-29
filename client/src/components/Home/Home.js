a762a1ab78bac1efde6625012569c2474aa8c69b
drumovski <jason.stacy@gmail.com>
2020-12-29 11:19:08 +1100

    add page styling

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,11 +1,19 @@
 import React from "react";
 import FindClass from "../FindClass/FindClass";
+import Heading from "../Heading/Heading";
+import Container from "react-bootstrap/Container"
 
 const Home = () => {
   return (
     <div>
-      <h1>Home</h1>
+      <Container className = "main-container">
+      {Heading("Home")}
+      <h5>“E Concrematio. Confirmatio - </h5>
+      <h5>out ot the fire comes firmness, 
+        through stress we pass to strength.”</h5>
+        <p>― Charles F. Binns </p>
       <FindClass />
+      </Container>
     </div>
   );
 };
