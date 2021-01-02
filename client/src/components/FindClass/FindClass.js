43983b6b36227192a85577f1166a9eb476f9667f
drumovski <jason.stacy@gmail.com>
2021-01-02 22:04:39 +1100

    remove container from find a class button

--- client/src/components/FindClass/FindClass.js
+++ client/src/components/FindClass/FindClass.js
@@ -1,17 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import ButtonComponent from "react-bootstrap/Button"
-import Container from "react-bootstrap/Container"
 
 
 const FindClass = () => {
   return (
     <div>
-    <Container>
       <Link to="/classes">
         <ButtonComponent variant="primary">Find a Class</ButtonComponent>
       </Link>
-    </Container>
     </div>
   );
 };
