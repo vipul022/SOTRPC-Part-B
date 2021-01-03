a36b3e7a405b47f6fe99086f4fd92d0f34c15959
vipul022 <vipul.seth022@gmail.com>
2021-01-04 10:29:10 +1100

    deleted the image form FindClass component

--- client/src/components/FindClass/FindClass.js
+++ client/src/components/FindClass/FindClass.js
@@ -1,21 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import ButtonComponent from "react-bootstrap/Button";
-import Container from "react-bootstrap/Container";
-import Image from "../../data/images/bg_image.png";
+import ButtonComponent from "react-bootstrap/Button"
+
 
 const FindClass = () => {
   return (
-    <div id="find-class-wrapper">
-      <Container fluid>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
-        <img src={Image} />
-        <div>
-          <Link to="/classes">
-            <ButtonComponent variant="primary">Find a Class</ButtonComponent>
-          </Link>
-        </div>
-      </Container>
+    <div>
+      <Link to="/classes">
+        <ButtonComponent variant="primary">Find a Class</ButtonComponent>
+      </Link>
     </div>
   );
 };
