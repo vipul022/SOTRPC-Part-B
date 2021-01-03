efe17aea3b754a9273167653d6184fbd354f911e
vipul022 <vipul.seth022@gmail.com>
2021-01-03 17:28:10 +1100

    added some basic style to Home and FindClass

--- client/src/components/FindClass/FindClass.js
+++ client/src/components/FindClass/FindClass.js
@@ -1,17 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import ButtonComponent from "react-bootstrap/Button"
-import Container from "react-bootstrap/Container"
-
+import ButtonComponent from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Image from "../../data/images/bg_image.png";
 
 const FindClass = () => {
   return (
-    <div>
-    <Container>
-      <Link to="/classes">
-        <ButtonComponent variant="primary">Find a Class</ButtonComponent>
-      </Link>
-    </Container>
+    <div id="find-class-wrapper">
+      <Container fluid>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+        <img src={Image} />
+        <div>
+          <Link to="/classes">
+            <ButtonComponent variant="primary">Find a Class</ButtonComponent>
+          </Link>
+        </div>
+      </Container>
     </div>
   );
 };
--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,17 +1,20 @@
 import React from "react";
 import FindClass from "../FindClass/FindClass";
 import Heading from "../Heading/Heading";
-import Container from "react-bootstrap/Container"
+import Container from "react-bootstrap/Container";
 
 const Home = () => {
   return (
     <div>
-      <Container className = "content-container">
-      <h5>“E Concrematio. Confirmatio - </h5>
-      <h5>out ot the fire comes firmness, 
-        through stress we pass to strength.”</h5>
+      {/* <Container className="content-container"> */}
+      <Container fluid>
+        <h5>“E Concrematio. Confirmatio - </h5>
+        <h5>
+          out ot the fire comes firmness, through stress we pass to strength.”
+        </h5>
         <p>― Charles F. Binns </p>
-      <FindClass />
+
+        <FindClass />
       </Container>
     </div>
   );
