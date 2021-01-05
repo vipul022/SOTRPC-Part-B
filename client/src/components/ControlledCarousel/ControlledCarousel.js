7d9e0bf54ec2c6c05b1b7133a4804f574acd5611
vipul022 <vipul.seth022@gmail.com>
2021-01-05 18:24:28 +1100

    code cleanup

--- client/src/components/ControlledCarousel/ControlledCarousel.js
+++ client/src/components/ControlledCarousel/ControlledCarousel.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
-import { useGlobalState } from "../../config/globalState";
-import { Carousel, Container, Row, Col, Image } from "react-bootstrap";
+
+import { Carousel, Image } from "react-bootstrap";
 
 function ControlledCarousel({ index, photos }) {
-  console.log("index is ", index)
+  console.log("index is ", index);
   const [photoIndex, setPhotoIndex] = useState(0);
 
   const handleSelect = (index, e) => {
@@ -15,18 +15,14 @@ function ControlledCarousel({ index, photos }) {
     photos.map((photo) => {
       console.log("photo inside content=>", photo);
       return (
-          <Carousel.Item key = {photo._id}>
-            <Image className = "photo"
-              src={photo.url}
-              alt={photo.description}
-            />
-            <Carousel.Caption>
-              <p>{photo.description}</p>
-            </Carousel.Caption>
-          </Carousel.Item>
+        <Carousel.Item key={photo._id}>
+          <Image className="photo" src={photo.url} alt={photo.description} />
+          <Carousel.Caption>
+            <p>{photo.description}</p>
+          </Carousel.Caption>
+        </Carousel.Item>
       );
     });
-
 
   return (
     <Carousel activeIndex={photoIndex} onSelect={handleSelect}>
