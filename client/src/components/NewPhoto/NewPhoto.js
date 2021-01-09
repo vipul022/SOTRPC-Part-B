976cf5d818ec2bb529fe20acaebcb1d7ecbf25f9
drumovski <jason.stacy@gmail.com>
2021-01-09 11:48:09 +1100

    refactor new photo and fix error messages

--- client/src/components/NewPhoto/NewPhoto.js
+++ client/src/components/NewPhoto/NewPhoto.js
@@ -1,10 +1,7 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
 import { uploadPhotoToS3 } from "../../services/photoServices";
-// import { useGlobalState } from "../../config/globalState";
 import Header from "../Header/Header";
 import { addNewPhoto } from "../../services/photoServices";
-import ButtonComponent from "../Button/Button";
 import { Form, Container, Row, Col, Button, Alert } from "react-bootstrap";
 
 // ! reference taken from https://medium.com/@khelif96/uploading-files-from-a-react-app-to-aws-s3-the-right-way-541dd6be689
@@ -19,17 +16,34 @@ const NewPhoto = ({ history }) => {
   const [PhotoState, setPhotoState] = useState(initialPhotoState);
   const [errorMessage, setErrorMessage] = useState(null);
 
+  const validatePhoto = (fileType, size) => {
+  const typeLowerCase = fileType.toLowerCase();
+  const TWOMEGABYTES = 2097152;
+   console.log("selectedFile.size=>", size);
+   console.log("fileType=>", fileType);
+   if (size >= TWOMEGABYTES) {
+    setErrorMessage("File Size needs to be below 2MB");
+    return false;
+   } else if (typeLowerCase !== "jpg" && typeLowerCase !== "jpeg" && typeLowerCase !== "png" ) {
+    setErrorMessage("File type needs to be a jpg or png");
+    return false;
+   } else {
+    setErrorMessage(null);
+    return true;
+   };   
+  }
+
+
 
 const uploadFile = (fileName, fileType) => {
-  const { success, description, selectedFile } = PhotoState;
+  const { description, selectedFile } = PhotoState;
    console.log("Preparing the upload");
   addNewPhoto({ fileName, fileType, description })
       .then((response) => {
-        // console.log("response=>", response);
         const { returnData } = response.data.data;
         const { signedRequest } = returnData;
-        const responsePhoto = response.data.photo
-        console.log("photo=>", responsePhoto)
+        const responsePhoto = response.data.photo;
+        console.log("photo=>", responsePhoto);
 
         setPhotoState({
           ...PhotoState,
@@ -38,8 +52,6 @@ const uploadFile = (fileName, fileType) => {
         });
         const id = responsePhoto._id;
         console.log("photoState now=>", PhotoState);
-        // var url = returnData.url;
-        // this.setState({ url: url });
         console.log("Recieved a signed request=> " + signedRequest);
         // Put the fileType in the headers for the upload
         var options = {
@@ -50,21 +62,22 @@ const uploadFile = (fileName, fileType) => {
         // !upload the photo to s3 bucket and incase of error delete the photo from db
         uploadPhotoToS3(signedRequest, selectedFile, options, id)
           .then((result) => {
-            console.log("result=>", result);
-            // this.setState({ success: true });
+            console.log(result);
             setPhotoState({
               ...PhotoState,
               success: true,
             });
           })
-          .catch((error) => console.log(error));
+          .catch((error) => {
+            console.log(error);
+            setErrorMessage("There was a problem saving the photo to S3");
+          });
       })
       .catch((error) => {
-        alert(JSON.stringify(error));
+        console.log(error);
+        setErrorMessage("There was a problem saving the photo to the server");
       });
 }
-
-
 
   const handleDescriptionChange = (event) => {
     const { name, value } = event.target;
@@ -74,38 +87,28 @@ const uploadFile = (fileName, fileType) => {
       success: false,
       url: "",
     });
-    console.log("inside handleDescriptionChange, photoState=>", PhotoState);
+    // console.log("inside handleDescriptionChange, photoState=>", PhotoState);
   };
 
   const handleFileChange = (event) => {
     setPhotoState({
       ...PhotoState,
       selectedFile: event.target.files[0],
+      success: false
     });
-    console.log("inside handleFileChange, photoState=>", PhotoState);
+    setErrorMessage(null);
+    // console.log("inside handleFileChange, photoState=>", PhotoState);
   };
 
   const handleUpload = (event) => {
     event.preventDefault();
-    const { success, description, selectedFile, photo } = PhotoState;
+    const {selectedFile} = PhotoState;
        // //! Split the filename to get the name and type
    let fileParts = selectedFile.name.split(".");
    let fileName = fileParts[0];
-   let fileType = fileParts[1].toLowerCase();
-   console.log("selectedFile.size=>", selectedFile.size)
-   console.log("fileType=>", fileType)
-   if (selectedFile.size >= 2097152) {
-    setErrorMessage("File Size needs to be below 2MB");
-   } else if (fileType !== "jpg" && fileType !== "jpeg" && fileType !== "png" ) {
-    setErrorMessage("File type needs to be a jpg or png")
-   } else {
-    setErrorMessage(null)
-    uploadFile(fileName, fileType)
+   let fileType = fileParts[1];
+   if (validatePhoto(fileType, selectedFile.size)) uploadFile(fileName, fileType);    
    };   
-  };
-
-    
-
 
   return (
       <Container className="small-container">
@@ -113,6 +116,10 @@ const uploadFile = (fileName, fileType) => {
         {errorMessage && (
           <Alert variant="danger">
             <p>{errorMessage}</p>
+          </Alert>)}
+          {PhotoState.success && (
+          <Alert variant='success'>
+            <p>Upload Successful!</p>
           </Alert>)}
         <Form onSubmit={handleUpload}>
 
@@ -139,123 +146,5 @@ const uploadFile = (fileName, fileType) => {
       </Container>
   );
 };
-// class NewPhoto extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       success: false,
-//       url: "",
-//       description: "",
-//       photo: {},
-//     };
-//     console.log("description=>", this.state.description);
-//   }
 
-//   handleChange = (ev) => {
-//     const { name, value } = ev.target;
-
-//     console.log("name==>", name);
-//     console.log("value=>", value);
-//     this.setState({ success: false, url: "", [name]: value });
-//     // console.log(this.state.description);
-//     console.log("inside handleChange=>");
-//   };
-
-//   handleUpload = (ev) => {
-//     const { history } = this.props;
-//     console.log("history=>", history);
-//     const { success, description } = this.state;
-//     console.log("success=>", success);
-
-//     console.log("uploadInput.files=>", this.uploadInput.files);
-//     let file = this.uploadInput.files[0];
-//     console.log("file=>", file);
-//     //! Split the filename to get the name and type
-//     let fileParts = file.name.split(".");
-//     console.log("fileParts=>", fileParts);
-//     let fileName = fileParts[0];
-//     let fileType = fileParts[1];
-//     console.log("Preparing the upload");
-
-//     addNewPhoto({ fileName, fileType, description })
-//       .then((response) => {
-//         console.log("response=>", response);
-//         var returnData = response.data.data.returnData;
-//         var signedRequest = returnData.signedRequest;
-//         // !Extracting id from response
-//         // const id = response.data.databaseId;
-//         this.setState({ photo: response.data.photo });
-//         const id = this.state.photo._id;
-//         // console.log("id=>", id);
-//         var url = returnData.url;
-//         this.setState({ url: url });
-//         console.log("Recieved a signed request=> " + signedRequest);
-//         // Put the fileType in the headers for the upload
-//         var options = {
-//           headers: {
-//             "Content-Type": fileType,
-//           },
-//         };
-//         // !upload the photo to s3 bucket and incase of error delete the photo from db
-//         uploadPhotoToS3(signedRequest, file, options, id)
-//           .then((result) => {
-//             console.log("result=>", result);
-//             this.setState({ success: true });
-//           })
-//           .catch((error) => console.log(error));
-//       })
-//       .catch((error) => {
-//         alert(JSON.stringify(error));
-//       });
-//     // history.push(`/photos`);
-//   };
-
-//   render() {
-//     const SuccessMessage = () => (
-//       <div style={{ padding: 50 }}>
-//         <h3 style={{ color: "green" }}>SUCCESSFUL UPLOAD</h3>
-
-//         <Link
-//           to={{
-//             pathname: `/photos/${this.state.photo._id}`,
-//             state: { photo: this.state.photo },
-//             // !sending photo as photo  to the pathname
-//           }}
-//         >
-//           Access the file here
-//         </Link>
-//         <br />
-//       </div>
-//     );
-//     return (
-//       <div>
-//         <center>
-//           <h1>UPLOAD A FILE</h1>
-//           {this.state.success ? <h3>SUCCESSFUL UPLOAD</h3> : null}
-//           <div>
-//             <label>Description</label>
-//             <input
-//               type="text"
-//               name="description"
-//               placeholder="Enter description..."
-//               maxLength="60"
-//               onChange={this.handleChange}
-//             ></input>
-//           </div>
-//           <div>
-//             <input
-//               onChange={this.handleChange}
-//               ref={(ref) => {
-//                 this.uploadInput = ref;
-//               }}
-//               type="file"
-//             />
-//           </div>
-//           <br />
-//           <ButtonComponent clicked={this.handleUpload}>UPLOAD</ButtonComponent>
-//         </center>
-//       </div>
-//     );
-//   }
-// }
 export default NewPhoto;
